c94a15f43b63350a8e46dc09a7b7a23bcc31b0b4
felarof <felarof.nayak@gmail.com>
2014-07-05 18:36:20 +0530

    check onLoad

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,8 +1,7 @@
-.onLoad <- function(lib,pkg){
+#.onLoad <- function(lib,pkg){
   #automatically loads the dataset when package is loaded
   #do not use this in combination with lazydata=true
-  #' @export
-  utils::data(partsDemand, package=pkg, envir = parent.env(environment()))
-}
+  #utils::data(partsDemand, package=pkg, envir = parent.env(environment()))
+#}
 #.onLoad(lib,"opencpuTS")
 
