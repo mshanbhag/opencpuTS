92fdf2a988f2fb0fd0e1e1e66e13f295d0c3f609
felarof <felarof.nayak@gmail.com>
2014-07-05 17:25:48 +0530

    with functions

--- R/Time Series Model.R
+++ R/Time Series Model.R
@@ -1,15 +1,31 @@
-partsDemand$Date = as.Date(partsDemand$Date, format = '%m/%d/%Y')
-Intera.Power.Pulsar = ts(partsDemand$Intera.1.5T.Power.Pulsar, frequency=12)
+forecast_stl<-function(input,plot.it=T){
 
-#arima.Intera.Power.Pulsar = auto.arima(ts.Intera.Power.Pulsar, trace=TRUE)
-#plot(forecast(arima.Intera.Power.Pulsar, h=10))
-#predict(arima.Intera.Power.Pulsar, n.ahead=10)
+#read in csv file as data frame with same format as partsDemand and pass it as argument to this function  
+  
+  input$Date = as.Date(input$Date, format = '%m/%d/%Y')
+  Intera.Power.Pulsar = ts(input$Intera.1.5T.Power.Pulsar, frequency=12)
+  
+  stl.Intera.Power.Pulsar = stl(Intera.Power.Pulsar, s.window='periodic')
+  if(plot.it=T){
+    plot(forecast(stl.Intera.Power.Pulsar, h=10))
+  }
+  predict.Intera.Power.Pulsar = data.frame(predict(stl.Intera.Power.Pulsar))
+  return(predict.Intera.Power.Pulsar)
+}
 
-library(forecast)
+decompose_stl<-function(input,plot.it=T){
+  
+  #read in csv file as data frame with same format as partsDemand and pass it as argument to this function  
+  
+  input$Date = as.Date(input$Date, format = '%m/%d/%Y')
+  Intera.Power.Pulsar = ts(input$Intera.1.5T.Power.Pulsar, frequency=12)
+  
+  stl.Intera.Power.Pulsar = stl(Intera.Power.Pulsar, s.window='periodic')
+  if(plot.it=T){
+    plot(stl.Intera.Power.Pulsar, main='Intera Power Pulsar')
+  }
+  df.Intera.Power.Pulsar = as.data.frame(stl.Intera.Power.Pulsar$time.series[,1:3])
+  return(predict.Intera.Power.Pulsar)
+}
 
-stl.Intera.Power.Pulsar = stl(Intera.Power.Pulsar, s.window='periodic')
-plot(stl.Intera.Power.Pulsar, main='Intera Power Pulsar')
-plot(forecast(stl.Intera.Power.Pulsar, h=10))
 
-predict.Intera.Power.Pulsar = data.frame(predict(stl.Intera.Power.Pulsar))
-df.Intera.Power.Pulsar = as.data.frame(stl.Intera.Power.Pulsar$time.series[,1:3])
